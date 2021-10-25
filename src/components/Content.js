8dd810675a95fb5f44f22fec7926e5a7ec822ddc
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-25 20:14:23 +0300

    Add content component

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <main className='content'>
+      <h1>Films</h1>
+      <h3>Title</h3>
+      <p>
+        Ipsum qui esse eu Lorem ipsum ad culpa in proident incididunt. Aute
+        cupidatat nisi officia veniam sunt nostrud mollit in dolor. Qui aliquip
+        ipsum ut cillum.
+      </p>
+    </main>
+  )
+}
+
+export default Content
