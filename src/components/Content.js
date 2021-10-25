b8c7ab296f89a2756a78434175b4f7fb6424bb98
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-25 21:07:14 +0300

    Fix refactor content

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Content = () => {
   return (
-    <main className='content'>
-      <h1>Films</h1>
-      <h3>Title</h3>
-      <p>
-        Ipsum qui esse eu Lorem ipsum ad culpa in proident incididunt. Aute
-        cupidatat nisi officia veniam sunt nostrud mollit in dolor. Qui aliquip
-        ipsum ut cillum.
-      </p>
-    </main>
+    <div className='content'>
+      <div className='film-content'>
+        <h1>Films</h1>
+        <h3>Title</h3>
+        <p>
+          Ipsum qui esse eu Lorem ipsum ad culpa in proident incididunt. Aute
+          cupidatat nisi officia veniam sunt nostrud mollit in dolor. Qui
+          aliquip ipsum ut cillum.
+        </p>
+      </div>
+    </div>
   )
 }
 
