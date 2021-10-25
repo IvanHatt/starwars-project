6c0a8625af980e0ddf0f59f4b50920f66f736114
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-25 20:14:51 +0300

    Add add components to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
+import Content from './components/Content'
+import Toc from './components/Toc'
 
 function App() {
   return (
-    <div className="App">
-     Hello World!
+    <div>
+      <Header></Header>
+      <main className='container-fluid'>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <Toc></Toc>
+          </div>
+          <div className='col-sm-8'>
+            <Content></Content>
+          </div>
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
