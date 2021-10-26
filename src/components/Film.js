1638b313af1d0944b04329045e4819923f15cac8
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 14:48:01 +0300

    Create create film component

---
+++ src/components/Film.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Film = ({ film }) => {
+  const { title, opening_crawl, director, release_date } = film
+  return (
+    <div className='film-content'>
+      <h2>{title}</h2>
+      <p>{opening_crawl}</p>
+      <p>Directed by {director}</p>
+      <p>Released on {release_date}</p>
+      <button
+        type='button'
+        className='btn btn-dark m-1'
+        data-bs-toggle='tooltip'
+        data-bs-placement='bottom'
+        title='Add to favorites'
+      >
+        <i className='bi bi-star'></i>
+      </button>
+      <button
+        type='button'
+        className='btn btn-dark m-1'
+        data-bs-toggle='tooltip'
+        data-bs-placement='bottom'
+        title='Remove from favorites'
+      >
+        <i className='bi bi-trash'></i>
+      </button>
+    </div>
+  )
+}
+
+export default Film
