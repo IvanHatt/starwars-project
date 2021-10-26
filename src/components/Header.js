b4f2773e3cb4189a739783b81e3ce30063fe8eb2
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 11:10:32 +0300

    Add add buton favorites

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,12 @@ const Header = () => {
     <header className='header'>
       <h1>Star Wars</h1>
       <div className='d-flex justify-content-end'>
-        <a className='favs' href='www.g.de'>
-          My Favorites
-        </a>
+        <button type='button' className='btn btn-dark mx-5 position-relative'>
+          <i className='bi bi-star m-1'></i> My favorites
+          <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary'>
+            2<span className='visually-hidden'>number of favorites</span>
+          </span>
+        </button>
       </div>
     </header>
   )
