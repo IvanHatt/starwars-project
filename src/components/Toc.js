fcb465a7908db157e286b40f57480715a95c5008
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 11:02:25 +0300

    Refactor add responsive toggle

--- src/components/Toc.js
+++ src/components/Toc.js
@@ -2,24 +2,66 @@ import React from 'react'
 
 const Toc = () => {
   return (
-    <div className='toc-container'>
-      <div className='d-flex flex-column flex-shrink-0 p-3 text-white'>
+    <nav className='navbar navbar-expand-lg d-flex flex-column justify-content-start toc-container'>
+      <div className='container-fluid'>
         <h2>Movie List</h2>
-        <hr></hr>
-        <ul className='nav nav-pills flex-column mb-auto'>
-          <li className='nav-item'>
-            <a href='www.g.de' className='nav-link active' aria-current='page'>
-              First
-            </a>
-          </li>
-          <li>
-            <a href='www.g.de' className='nav-link text-white'>
-              Second
-            </a>
-          </li>
-        </ul>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#toc'
+          aria-controls='toc'
+          aria-expanded='false'
+          aria-label='Toggle Toc'
+        >
+          <i className='bi bi-list'></i>
+        </button>
       </div>
-    </div>
+      <div className='container-fluid'>
+        <div className='collapse navbar-collapse p-3' id='toc'>
+          <ul className='nav nav-pills flex-column'>
+            <li className='nav-item'>
+              <a
+                href='www.g.de'
+                className='nav-link active'
+                aria-current='page'
+              >
+                A New Hope
+              </a>
+            </li>
+            <li>
+              <a href='www.g.de' className='nav-link text-white'>
+                Second
+              </a>
+            </li>
+            <li>
+              <a href='www.g.de' className='nav-link text-white'>
+                Second
+              </a>
+            </li>
+            <li>
+              <a href='www.g.de' className='nav-link text-white'>
+                Second
+              </a>
+            </li>
+            <li>
+              <a href='www.g.de' className='nav-link text-white'>
+                Second
+              </a>
+            </li>
+            <li>
+              <a href='www.g.de' className='nav-link text-white'>
+                Second
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* <div className='toc-container'>
+      
+    </div> */}
+    </nav>
   )
 }
 
