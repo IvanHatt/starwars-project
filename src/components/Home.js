727e5575aaae7bb85f4c1b1efa0709755ce26e25
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 14:47:45 +0300

    Create create home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='film-content text-center'>
+      <h2>Welcome to Star Wars Movies list</h2>
+      <h3>May the force be with you!</h3>
+    </div>
+  )
+}
+
+export default Home
