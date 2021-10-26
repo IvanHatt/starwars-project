930ba5a19d77df44c66d923ef44113e1bf6c05fc
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 16:45:58 +0300

    Add add fav functionality

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 import Spinner from './Spinner'
 import Film from './Film'
 import Home from './Home'
+import FavsList from './FavsList'
 
 const Content = () => {
   let location = useLocation()
   const [film, setFilm] = useState([])
+  const [favs, setFavs] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -24,14 +26,44 @@ const Content = () => {
     fetchFilm()
   }, [location])
 
+  useEffect(() => {
+    const favsStored = JSON.parse(localStorage.getItem('favorites-list'))
+    setFavs(favsStored)
+  }, [setFavs])
+
+  const addFav = (film) => {
+    const newFavList = [...favs, film]
+    setFavs(newFavList)
+    saveToLocalStorage(newFavList)
+  }
+
+  const saveToLocalStorage = (favs) => {
+    localStorage.setItem('favorites-list', JSON.stringify(favs))
+  }
+
   return (
     <div className='content'>
+      <div className='d-flex justify-content-end my-5'>
+        <Link
+          type='button'
+          className='btn btn-dark mx-5 position-relative'
+          to='/favs'
+        >
+          <i className='bi bi-star m-1'></i> My favorites
+          <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary'>
+            {favs.length}
+            <span className='visually-hidden'>number of favorites</span>
+          </span>
+        </Link>
+      </div>
       {location.pathname === '/' ? (
         <Home />
+      ) : location.pathname === '/favs' ? (
+        <FavsList favs={favs} />
       ) : isLoading ? (
         <Spinner />
       ) : (
-        <Film film={film}></Film>
+        <Film film={film} handleFav={addFav}></Film>
       )}
     </div>
   )
