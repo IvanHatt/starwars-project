ada0d65ec7212e58603a0d9b3f40b7da5dd7437e
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 14:48:22 +0300

    Refactor refactor content to accomodate components

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,45 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { useLocation } from 'react-router-dom'
+import Spinner from './Spinner'
+import Film from './Film'
+import Home from './Home'
 
 const Content = () => {
   let location = useLocation()
-  console.log(location)
+  const [film, setFilm] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchFilm = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://swapi.dev/api/films${location.pathname}`
+      )
+
+      setFilm(result.data)
+      setIsLoading(false)
+    }
+
+    fetchFilm()
+  }, [location])
+
   return (
     <div className='content'>
-      <div className='film-content'>
-        <h1>Films</h1>
-        <h3>A New Hope</h3>
-        <p>
-          It is a period of civil war. Rebel spaceships, striking from a hidden
-          base, have won their first victory against the evil Galactic Empire.
-          During the battle, Rebel spies managed to steal secret plans to the
-          Empire's ultimate weapon, the DEATH STAR, an armored space station
-          with enough power to destroy an entire planet. Pursued by the Empire's
-          sinister agents, Princess Leia races home aboard her starship,
-          custodian of the stolen plans that can save her people and restore
-          freedom to the galaxy....
-        </p>
-        <p>Directed by</p>
-        <p>Released on</p>
-        <button
-          type='button'
-          className='btn btn-dark m-1'
-          data-bs-toggle='tooltip'
-          data-bs-placement='bottom'
-          title='Add to favorites'
-        >
-          <i className='bi bi-star'></i>
-        </button>
-        <button
-          type='button'
-          className='btn btn-dark m-1'
-          data-bs-toggle='tooltip'
-          data-bs-placement='bottom'
-          title='Remove from favorites'
-        >
-          <i className='bi bi-trash'></i>
-        </button>
-      </div>
+      {location.pathname === '/' ? (
+        <Home />
+      ) : isLoading ? (
+        <Spinner />
+      ) : (
+        <Film film={film}></Film>
+      )}
     </div>
   )
 }
