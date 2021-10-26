a6748ad701d778320d571e2b09e7a88b2844957f
Ivan Hattemer <ivanhattemer@gmail.com>
2021-10-26 16:45:40 +0300

    Refactor delete button add to fav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,6 @@ const Header = () => {
   return (
     <header className='header'>
       <h1>Star Wars</h1>
-      <div className='d-flex justify-content-end'>
-        <button type='button' className='btn btn-dark mx-5 position-relative'>
-          <i className='bi bi-star m-1'></i> My favorites
-          <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary'>
-            2<span className='visually-hidden'>number of favorites</span>
-          </span>
-        </button>
-      </div>
     </header>
   )
 }
